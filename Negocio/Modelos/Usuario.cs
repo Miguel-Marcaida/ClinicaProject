6fa2485fff2a953df8095e1c6fe0055220594711
Miguel Marcaida <miguelangelmarcaida@gmail.com>
2026-03-31 19:09:45 -0300

    feat: creacion de clase usuario

---
+++ Negocio/Modelos/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Modelos
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+        public bool Activo { get; set; }
+        //datos de la persona vinculada
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }    
+    }                                       
+}
