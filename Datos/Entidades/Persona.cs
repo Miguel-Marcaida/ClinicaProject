3d27c2a43f9112393f547154bef08dc05899aae2
Miguel Marcaida <miguelangelmarcaida@gmail.com>
2026-03-31 22:22:01 -0300

    Entidad Persona

---
+++ Datos/Entidades/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Entidades
+{
+    public class Persona
+    {
+        public int id_persona { get; set; }
+        public string dni { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fecha_nacimiento { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+
+        public Persona() { }
+
+
+        public string NombreCompleto() => $"{nombre} {apellido}";
+    }
+}
